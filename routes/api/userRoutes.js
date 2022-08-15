299fa049d590578e79a20310d8c450594d995f2d
Zachary-Maddox <z19s96p@aol.com>
2022-08-15 18:19:30 -0500

    added get one by id

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,10 +5,33 @@ router.get('/', (req, res) => {
   User.find().then( (data) => {
     res.json(data)
 
-  }) 
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 })
 
+router.post('/', (req, res) => {
+  User.create(req.body).then( (data) => {
+    
+    res.json(data)
 
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
+
+router.get('/:id', (req, res) => {
+  User.findOne({_id:req.params.id}).then( (data) => {
+  
+    res.json(data)
+
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
 
 
 
