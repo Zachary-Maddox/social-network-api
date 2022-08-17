4579d4baba6c8e9ff4315f0d210c6526843bbe6d
Zachary-Maddox <z19s96p@aol.com>
2022-08-16 19:46:28 -0500

    fixed bugs with add/delete reactions to thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,98 +1,120 @@
-const router = require('express').Router();
-const Thought = require('../../models/Thought');
-const User = require('../../models/User');
-const Reaction = require('../../models/Reaction');
+const router = require("express").Router();
+const Thought = require("../../models/Thought");
+const User = require("../../models/User");
+const Reaction = require("../../models/Reaction");
 
 // find all thoughts
-router.get('/', (req, res) => {
-  Thought.find().then( (data) => {
-    res.json(data)
-
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-})
+router.get("/", (req, res) => {
+    Thought.find()
+        .populate("reactions")
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 // create Thought
-router.post('/', (req, res) => {
-  Thought.create(req.body)
-      .then((dbThoughtData) => {
-        return User.findOneAndUpdate(
-          { _id: req.body.userId },
-          { $push: { thoughts: dbThoughtData._id } },
-          { new: true }
-        );
-      })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          return res.status(404).json({ message: 'Thought created but no user id matched!' });
-        }
+router.post("/", (req, res) => {
+    Thought.create(req.body)
+        .then((dbThoughtData) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: { thoughts: dbThoughtData._id } },
+                { new: true }
+            );
+        })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res
+                    .status(404)
+                    .json({
+                        message: "Thought created but no user id matched!",
+                    });
+            }
 
-        res.json({ message: 'Thought created successfully!' });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-})
+            res.json({ message: "Thought created successfully!" });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 // find thought by id
-router.get('/:id', (req, res) => {
-  Thought.findOne({_id:req.params.id}).then( (data) => {
-  
-    res.json(data)
-
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-})
+router.get("/:id", (req, res) => {
+    Thought.findOne({ _id: req.params.id })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // Update thought by Id
 router.put("/update/:thoughtId", (req, res) => {
-  Thought.findOneAndUpdate({ _id: req.params.thoughtId},{$set:req.body} ,{new:true,runValidators:true})
-      .then((data) => {
-          res.json(data);
-      })
-      .catch((err) => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { new: true, runValidators: true }
+    )
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // delete a thought by id
 router.delete("/delete/:id", (req, res) => {
-  Thought.findOneAndDelete({ _id: req.params.id })
-      .then((data) => {
-          res.json(data);
-      })
-      .catch((err) => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+    Thought.findOneAndDelete({ _id: req.params.id })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // add a reaction to thought
-router.put("/add/:thoughtId/:reactionId", (req, res) => {
-  Thought.findOneAndUpdate({ _id: req.params.thoughtId},{$set:req.body} ,{new:true,runValidators:true})
-      .then((data) => {
-          res.json(data);
-      })
-      .catch((err) => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+router.put("/reaction/add/:thoughtId", (req, res) => {
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { new: true, runValidators: true }
+    )
+        .then((data) => {
+            if (!data)
+                return res.json({
+                    message: "There was no thought find by that Id",
+                });
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // remove reaction from thought
 router.put("/remove/:thoughtId/:reactionId", (req, res) => {
-  Thought.findOneAndUpdate({ _id: req.params.thoughtId},{$pull:{reaction:req.params.reactionId}},{new:true})
-      .then((data) => {
-          res.json(data);
-      })
-      .catch((err) => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions:{_id:req.params.reactionId} } },
+        { new: true }
+    )
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-module.exports = router
+module.exports = router;
